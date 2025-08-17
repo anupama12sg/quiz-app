3f3eb023861ef6d219f2e1395dc745c40f385b84
Aditya <anupama12sg@gmail.com>
2025-08-17 20:27:44 +0530

    added question & its options

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import './Quiz.css'
 
 const Quiz = () => {
-  return (
-    <div className='container'>
-      <h1>Quiz App</h1>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <h1>Quiz App</h1>
+            <hr />
+            <h2>Which is a NOT a cryptocurrency?</h2>
+            <ul>
+                <li>Bitcoin</li>
+                <li>Blockchain</li>
+                <li>Ethereum</li>
+                <li>cUSD</li>
+            </ul>
+        </div>
+    )
 }
 
 export default Quiz
