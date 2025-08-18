0e850a640229884d31e9f64fb0dc5f69041dead7
Aditya <anupama12sg@gmail.com>
2025-08-19 01:25:54 +0530

    added questions to the object

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,3 +1,39 @@
 export const data = [
-    {}, {}, {}, {}, {}
+    {
+        question: "Which is a NOT a cryptocurrency?",
+        option1: "Bitcoin",
+        option1: "Blockchain",
+        option1: "Ethereum",
+        option1: "cUSD",
+        ans: "2"
+    }, 
+{
+        question: "Founder of Ethereum is",
+        option1: "Satashi Nakamoto",
+        option1: "Larry Page",
+        option1: "Vitalik Buterin",
+        option1: "Aya Miyaguchi",
+        ans: "3"
+    }, {
+        question: "Which one is a stablecoin?",
+        option1: "USDT",
+        option1: "SOL",
+        option1: "ETH",
+        option1: "BTC",
+        ans: "1"
+    }, {
+        question: "The phenomenon where Ethereum shifted its consensus from Proof of Work to Proof of Stake is called",
+        option1: "The Purge",
+        option1: "The Merge",
+        option1: "The Splurge",
+        option1: "The Cascade effect",
+        ans: "2"
+    }, {
+        question: "Which blockchain is having fastest transaction per second?",
+        option1: "Bitcoin",
+        option1: "Solana",
+        option1: "Ethereum",
+        option1: "Polygon",
+        ans: "2"
+    }, 
 ]
