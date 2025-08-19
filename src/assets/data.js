ddc6f4ab68b2e32e950905f1fc6e208fa40c6c22
Aditya <anupama12sg@gmail.com>
2025-08-19 18:20:00 +0530

    chore: bg color of the options changing now

--- src/assets/data.js
+++ src/assets/data.js
@@ -5,7 +5,7 @@ export const data = [
         option2: "Blockchain",
         option3: "Ethereum",
         option4: "cUSD",
-        ans: "2"
+        ans: 2
     }, 
 {
         question: "Founder of Ethereum is",
@@ -13,27 +13,27 @@ export const data = [
         option2: "Larry Page",
         option3: "Vitalik Buterin",
         option4: "Aya Miyaguchi",
-        ans: "3"
+        ans: 3
     }, {
         question: "Which one is a stablecoin?",
         option1: "USDT",
         option2: "SOL",
         option3: "ETH",
         option4: "BTC",
-        ans: "1"
+        ans: 1
     }, {
         question: "The phenomenon where Ethereum shifted its consensus from Proof of Work to Proof of Stake is called",
         option1: "The Purge",
         option2: "The Merge",
         option3: "The Splurge",
         option4: "The Cascade effect",
-        ans: "2"
+        ans: 2
     }, {
         question: "Which blockchain is having fastest transaction per second?",
         option1: "Bitcoin",
         option2: "Solana",
         option3: "Ethereum",
         option4: "Polygon",
-        ans: "2"
+        ans: 2
     }, 
 ]
