8af6fac3109c4bb26a2256980d6192efa37124c9
Aditya <anupama12sg@gmail.com>
2025-08-19 17:07:18 +0530

    use of props

--- src/assets/data.js
+++ src/assets/data.js
@@ -2,38 +2,38 @@ export const data = [
     {
         question: "Which is a NOT a cryptocurrency?",
         option1: "Bitcoin",
-        option1: "Blockchain",
-        option1: "Ethereum",
-        option1: "cUSD",
+        option2: "Blockchain",
+        option3: "Ethereum",
+        option4: "cUSD",
         ans: "2"
     }, 
 {
         question: "Founder of Ethereum is",
         option1: "Satashi Nakamoto",
-        option1: "Larry Page",
-        option1: "Vitalik Buterin",
-        option1: "Aya Miyaguchi",
+        option2: "Larry Page",
+        option3: "Vitalik Buterin",
+        option4: "Aya Miyaguchi",
         ans: "3"
     }, {
         question: "Which one is a stablecoin?",
         option1: "USDT",
-        option1: "SOL",
-        option1: "ETH",
-        option1: "BTC",
+        option2: "SOL",
+        option3: "ETH",
+        option4: "BTC",
         ans: "1"
     }, {
         question: "The phenomenon where Ethereum shifted its consensus from Proof of Work to Proof of Stake is called",
         option1: "The Purge",
-        option1: "The Merge",
-        option1: "The Splurge",
-        option1: "The Cascade effect",
+        option2: "The Merge",
+        option3: "The Splurge",
+        option4: "The Cascade effect",
         ans: "2"
     }, {
         question: "Which blockchain is having fastest transaction per second?",
         option1: "Bitcoin",
-        option1: "Solana",
-        option1: "Ethereum",
-        option1: "Polygon",
+        option2: "Solana",
+        option3: "Ethereum",
+        option4: "Polygon",
         ans: "2"
     }, 
 ]
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -11,12 +11,12 @@ const Quiz = () => {
         <div className='container'>
             <h1>Quiz App</h1>
             <hr />
-            <h2>1. Which is a NOT a cryptocurrency?</h2>
+            <h2>{index+1}. {question.question}</h2>
             <ul>
-                <li>Bitcoin</li>
-                <li>Blockchain</li>
-                <li>Ethereum</li>
-                <li>cUSD</li>
+                <li>{question.option1}</li>
+                <li>{question.option2}</li>
+                <li>{question.option3}</li>
+                <li>{question.option4}</li>
             </ul>
             <button>Next</button>
             <div className='index'>1 of 5 questions</div>
